f8a9671a485085c0ef3152f4de766c5bbb3c60c9
Brian Murphy <brianmurphy@bmtechart.com>
2023-09-24 15:30:45 -0600

    Added cinemachine camera
    -added acceleration/deceleration to movement
    -added basic resource deposit prefab, still no harvesting implemented

--- Assets/Scripts/PlayerMovementBehaviour.cs
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -7,24 +7,34 @@ public class PlayerMovementBehaviour : MonoBehaviour
 {
     #region User Paramters
     [Header("Movement")]
+    [Tooltip("The speed the player will move when they have fully accelerated.")]
     [SerializeField]
-    private float movementSpeed;
+    private float maxSpeed = 5.0f;
+
+    [Tooltip("The rate the player accelerates towards max speed once movement input is received.")]
     [SerializeField]
-    private float targetMovementSpeed;
+    private float accelerationRate = 1.0f;
+
+    [Tooltip("The rate the player decelerates once ther eis no movement input")]
     [SerializeField]
-    private float accelerationRate;
+    private float decelerationRate = 1.0f;
+
+    [Tooltip("Flat multiplier for all speed attributes. Use if you want to scale this behaviour uniformly.")]
     [SerializeField]
-    private float decelerationRate;
+    private float speedMultiplier = 0.0f;
+
+    [Tooltip("Rate the player will turn to face the direction of movement input.")]
     [SerializeField]
-    private float speedMultiplier;
-    [SerializeField]
-    private float turnSpeed = 0.00001f;
+    private float turnSpeed = 0.1f;
     // Start is called before the first frame update
     #endregion
 
     #region Variables
 
     private Vector3 _movementInput;
+    private Vector3 _targetMoveVector;
+    private Vector3 _moveVector;
+    private float movementSpeed;
 
     #endregion
 
@@ -32,11 +42,21 @@ public class PlayerMovementBehaviour : MonoBehaviour
     private Rigidbody _rigidbody;
     #endregion
 
+    #region Runtime Callbacks
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
+    void Update()
+    {
+        AcceleratePlayer();
+        MovePlayer();
+        RotatePlayer();
+    }
 
+    #endregion
+
+    #region Methods
     public void GetMovementInput(Vector2 input)
     {
         Debug.Log(input);
@@ -44,20 +64,41 @@ public class PlayerMovementBehaviour : MonoBehaviour
     }
 
 
-
-    // Update is called once per frame
-    void Update()
+    private void RotatePlayer()
     {
-        //accelerate/decelerate towards target speed
-        //Vector3 targetPosition = transform.position+_movementInput;
-        _rigidbody.MovePosition(_rigidbody.position+_movementInput * movementSpeed * Time.deltaTime);
-
-        if(_movementInput.sqrMagnitude > 0.01f)
+        //if moving, rotate player to face the direction they are moving
+        if (_movementInput.sqrMagnitude > 0.01f)
         {
             Quaternion rotation = Quaternion.Slerp(_rigidbody.rotation,
                 Quaternion.LookRotation(_movementInput), turnSpeed);
             _rigidbody.MoveRotation(rotation);
         }
-        //move based on current speed
     }
+
+    private void MovePlayer()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + _moveVector * Time.deltaTime);
+    }
+
+    private void AcceleratePlayer()
+    {
+        //if moving, accelerate to max speed
+        if (_movementInput.sqrMagnitude > 0.01f)
+        {
+            movementSpeed = Mathf.Lerp(movementSpeed, maxSpeed, accelerationRate*Time.deltaTime); //linearly ramp to max speed
+            movementSpeed = Mathf.Clamp(movementSpeed, 0, maxSpeed); //safety
+
+            _moveVector = _movementInput * movementSpeed;
+            _targetMoveVector = _movementInput * maxSpeed;
+            _moveVector = Vector3.Lerp(_moveVector, _targetMoveVector, accelerationRate*Time.deltaTime); //ramp up to max move vector
+
+            return;
+        }
+
+        //if no movement input, decelerate back down to no movement
+        _moveVector = Vector3.Lerp(_moveVector, new Vector3(0,0,0), decelerationRate * Time.deltaTime); //ramp down to 0 movement
+    }
+
+    #endregion
+
 }
