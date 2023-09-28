9014f0c458a51ecceb301f1c263bd86df4005d8f
Brian Murphy <brianmurphy@bmtechart.com>
2023-09-28 10:58:53 -0600

    added notes to day night cycle behaviour

--- Assets/Scripts/DayNightCycleBehaviour.cs
+++ Assets/Scripts/DayNightCycleBehaviour.cs
@@ -11,13 +11,16 @@ public class DayNightCycleBehaviour : MonoBehaviour
     private float timeOfDay = 0.0f;
     private bool isDaytime = true;
 
-    public Action OnDayStart;
-    public Action OnNightStart;
+    public Action OnDayStart; //replace with unity events to show events in editor
+    public Action OnNightStart; // see docs for unity events here https://www.youtube.com/watch?v=ax1DiSutEy8
+    //create another event that fires every frame OnDayUpdate
+    //this event will require a float intput
+    //declare like this: public UnityEvent<float> OnDayUpdate;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartDayNightCycle();
+        StartDayNightCycle(); //replace with StartDay(), see notes above your functions to see what I mean
     }
 
     // Update is called once per frame
@@ -29,14 +32,24 @@ public class DayNightCycleBehaviour : MonoBehaviour
         {
             SwitchToNight();
             Debug.Log("It's night");
+            //return;
         }
-        else if(!isDaytime && timeOfDay >= nightDuration)
+        else if(!isDaytime && timeOfDay >= nightDuration) //style note: "else" isn't necessary here, just use two if statements
         {
             SwitchToDay();
             Debug.Log("It's day");
+            //return;
         }
+
+        //if
+        //Call OnDayUpdate(timeofDay/dayDuration or nightDuration based on if its day or night)
+        //this will give certain objects the time of day/night we are at expressed as a value between 0 and 1
+       
     }
 
+    //start DayNightCycle and SwitchToDay are the same function
+    //just use two functions, StartDay and StartNight instead of SwitchToDay and SwitchToNight
+    //otherwise this looks good
     private void StartDayNightCycle()
     {
         timeOfDay = 0.0f;
