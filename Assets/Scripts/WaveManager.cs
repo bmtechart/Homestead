de6b551c57e3ba286df806b3d74ec7750a7a14a7
Ben <b.benton287@mybvc.ca>
2023-10-16 10:44:03 -0600

    Fixed Wave Manager

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -5,87 +5,88 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    /*
-[SerializeField]
-private GameObject SpawnPoint;
+    [SerializeField]
+    private float countdown;
 
-[SerializeField]
-private float countdown;
+    public Wave[] waves;
 
-public Wave[] waves;
-
-public int CurrentWave = 0;
+    public int CurrentWave = 0;
 
 
-private bool ReadyCountDown; 
-
-private void Start()
-{
-    ReadyCountDown = true;
-
-    for (int i = 0; i < waves.Length; i++)
-    {
-        //waves[i].EnemeyCount = waves[i].enemies.Length;
-    }
-}
-
-private void Update()
-{
-
-    if (CurrentWave >= waves.Length)
-    {
-        //End of the game
-        return;
-    }
+    public Transform[] Spawnpoints;
 
 
-    if (ReadyCountDown == true)
-    {
-        countdown -= Time.deltaTime;
-    }
 
+    private bool ReadyCountDown;
 
-    if (countdown <= 0 )
-    {
-        ReadyCountDown = false;
-
-        countdown = waves[CurrentWave].NextWaveTime;
-        StartCoroutine(SpawnWave());
-    }
-
-    if (waves[CurrentWave].EnemeyCount == 0)
+    private void Start()
     {
         ReadyCountDown = true;
 
-        CurrentWave++;
-    }
-}
-
-private IEnumerator SpawnWave()
-{
-    if (CurrentWave < waves.Length)
-    {
-        for (int i = 0; i < waves[CurrentWave].enemies.Length; i++)
+        for (int i = 0; i < waves.Length; i++)
         {
-            //Enemy enemy = Instantiate(waves[CurrentWave].enemies[i], SpawnPoint.transform);
-
-            enemy.transform.SetParent(SpawnPoint.transform);
-
-            yield return new WaitForSeconds(waves[CurrentWave].BetweenEnemiesTime);
+            waves[i].EnemeyCount = waves[i].enemies.Length;
         }
     }
-}
+
+    private void Update()
+    {
+
+        if (CurrentWave >= waves.Length)
+        {
+            //End of the game
+            return;
+        }
+
+
+        if (ReadyCountDown == true)
+        {
+            countdown -= Time.deltaTime;
+        }
+
+
+        if (countdown <= 0)
+        {
+            ReadyCountDown = false;
+
+            countdown = waves[CurrentWave].NextWaveTime;
+            StartCoroutine(SpawnWave());
+        }
+
+        if (waves[CurrentWave].EnemeyCount == 0)
+        {
+            ReadyCountDown = true;
+
+            CurrentWave++;
+        }
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        if (CurrentWave < waves.Length)
+        {
+            for (int i = 0; i < waves[CurrentWave].enemies.Length; i++)
+            {
+                Transform Spawn = Spawnpoints[Random.Range(0, Spawnpoints.Length)];
+
+                AIController enemy = Instantiate(waves[CurrentWave].enemies[i], Spawn.transform);
+
+                enemy.transform.SetParent(Spawn.transform);
+
+                yield return new WaitForSeconds(waves[CurrentWave].BetweenEnemiesTime);
+            }
+        }
+    }
 
 
 
-[System.Serializable]
-public class Wave
-{
-    //public Enemy[] enemies;
-    public float BetweenEnemiesTime;
-    public float NextWaveTime;
+    [System.Serializable]
+    public class Wave
+    {
+        public AIController[] enemies;
+        public float BetweenEnemiesTime;
+        public float NextWaveTime;
 
-    public int EnemeyCount;
-}
- */
+        public int EnemeyCount;
+    }
 }
