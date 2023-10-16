9b756027de3de680c08b0aa9e51e1083f2deb573
Brian Murphy <brianmurphy@bmtechart.com>
2023-10-16 02:36:18 -0600

    Revert "added game start/over events to game manager"
    This reverts commit c5d1263c88fee2a4e75f9ca11a9a34873a1cf385.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,17 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class GameManager : Singleton<GameManager>
 {
-
-    #region Events
-
-    public UnityEvent m_OnGameOver;
-    public UnityEvent m_OnGameStart;
-
-    #endregion
 
     private PlayerController playerController;
     private CursorController cursorController;
@@ -40,8 +32,6 @@ public class GameManager : Singleton<GameManager>
 
         return playerController;
     }
-
-    
 
     // Update is called once per frame
     void Update()
